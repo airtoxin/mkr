588fdb51e4bfaad3fb6e9805603cdd025891a391
airtoxin <airtoxin@icloud.com>
2019-04-23 00:23:52 +0900

    Use emotion

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,31 @@
-import { jsx } from "@emotion/core";
+import {css, Global, jsx} from "@emotion/core";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { ConnectedRouter } from "connected-react-router";
 import { hist } from "./history";
 import { Routes } from "./Routes";
-import { setupGlobalCss } from "./boot/globalCss";
 
 const APP_ID = "app";
-
-setupGlobalCss(APP_ID);
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={hist}>
       {[
+        <Global
+          styles={css({
+            [`html, body, #${APP_ID}`]: {
+              height: "100vh",
+              width: "100vw",
+              padding: 0,
+              margin: 0
+            },
+            [`#${APP_ID}`]: {
+              display: "flex",
+              flexDirection: "column"
+            }
+          })}
+        />,
         <Routes />
       ]}
     </ConnectedRouter>
